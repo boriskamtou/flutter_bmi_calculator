d46423c70cd4b46a7f9fe166d71c89c57e6d61c1
kamtou boris <boriskamtou@gmail.com>
2019-07-11 03:26:29 +0200

    first part end

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,23 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(HomePage());
 
+enum selectedGender { male, female }
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  Color inactiveColor = Colors.transparent;
+  Color activeColorOne = Color(0xFF7b63c6);
+  Color activeColorTwo = Color(0xFF5237b5);
+
+  Color colorInit = Colors.transparent;
+
+  bool isSelected = false;
+  selectedGender gender;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -80,29 +91,108 @@ class _HomePageState extends State<HomePage> {
                       height: 10.0,
                     ),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              width: 1.3,
-                              color: Color(0xFFDEDEDE),
-                              style: BorderStyle.solid,
-                            ),
-                            borderRadius: BorderRadius.circular(6.0),
-                          ),
-                          height: 50.0,
-                          width: 120.0,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Icon(
-                                FontAwesomeIcons.male,
-                                color: Color(0xFFDFDEDF),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              gender = selectedGender.male;
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: gender == selectedGender.male
+                                    ? [activeColorOne, activeColorTwo]
+                                    : [Colors.transparent, Colors.transparent],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                stops: [0.6, 0.4],
                               ),
-                              Text('Male'),
-                            ],
+                              border: Border.all(
+                                width: 1.3,
+                                color: Color(0xFFDEDEDE),
+                                style: BorderStyle.solid,
+                              ),
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                            height: 50.0,
+                            width: 120.0,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(
+                                  FontAwesomeIcons.male,
+                                  size: 35.0,
+                                  color: Color(0xFFDFDEDF),
+                                ),
+                                SizedBox(
+                                  width: 4.0,
+                                ),
+                                Text(
+                                  'Male',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: gender == selectedGender.male
+                                        ? Colors.white
+                                        : Colors.grey.shade500,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        )
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              gender = selectedGender.female;
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: gender == selectedGender.female
+                                    ? [activeColorOne, activeColorTwo]
+                                    : [Colors.transparent, Colors.transparent],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                stops: [0.6, 0.4],
+                              ),
+                              border: Border.all(
+                                width: 1.3,
+                                color: Color(0xFFDEDEDE),
+                                style: BorderStyle.solid,
+                              ),
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                            height: 50.0,
+                            width: 120.0,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(
+                                  FontAwesomeIcons.female,
+                                  size: 35.0,
+                                  color: Color(0xFFDFDEDF),
+                                ),
+                                SizedBox(
+                                  width: 4.0,
+                                ),
+                                Text(
+                                  'Female',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    color: gender == selectedGender.female
+                                        ? Colors.white
+                                        : Colors.grey.shade500,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
                       ],
                     )
                   ],
